d58c8c74a015b9a01e3f3b75fc775d8d0b5e3eae
Alejok28 <alejo_k28@hotmail.com>
2018-05-05 11:53:55 -0500

    styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'municipalities#index'
   resources :municipalities
   resources :regions
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
